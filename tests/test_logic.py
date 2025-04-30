4e7f8732843424394a14fb83821428ef8f957220
name <raleksandrovic619@gmail.com>
2025-04-30 12:10:34 +0300

    исправление 3

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,17 +1,20 @@
 """Модуль тестирования логики функции вычисления квадратного уравнения"""
 import unittest
-from logic import kvur  
+from logic import kvur
 
 
 class TestKvurFunction(unittest.TestCase):
+    """тест"""
 
     def test_discriminant_positive(self):
-        # Пример уравнения: x^2 - 3x + 2 = 0, D = 1, корни 2 и 1
+        """тест"""
         result = kvur(1, -3, 2)
-        self.assertEqual(result[0], "Квадратное уравнение. D >0. Два корня уравнения")
+        self.assertEqual(
+            result[0],
+            "Квадратное уравнение. D >0. Два корня уравнения")
         self.assertEqual(result[1], 1)  # D
-        roots = sorted([result[2], result[3]])  # Сортировка, если порядок корней поменяется
+        roots = sorted([
+            result[2],
+            result[3]])
         self.assertAlmostEqual(roots[0], 1.0)
         self.assertAlmostEqual(roots[1], 2.0)
-
-
